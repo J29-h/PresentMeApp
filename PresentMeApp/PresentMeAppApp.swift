b6c00c5d2f78420dfad5f6e88d8e7732b94c87d0
Jana Malibari <Konderyano.0o0@hotmail.com>
2025-09-29 10:01:05 +0300

    Initial Commit

---
+++ PresentMeApp/PresentMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresentMeAppApp.swift
+//  PresentMeApp
+//
+//  Created by Jana Abdulaziz Malibari on 07/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct PresentMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
