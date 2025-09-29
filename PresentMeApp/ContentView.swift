f763a53bad157c27cc86b48f2212b9cb8f063cc3
Jana Malibari <Konderyano.0o0@hotmail.com>
2025-09-29 11:10:37 +0300

    Changed

--- PresentMeApp/ContentView.swift
+++ PresentMeApp/ContentView.swift
@@ -9,12 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+        VStack (alignment: .leading){
+            ZStack {
+                Color.red
+                    .frame(width: 370, height: 370)
+                    .cornerRadius(400)
+                Image("SPIDERPIC")
+                    .resizable()
+                    .frame(width: 350, height: 350)
+                    .cornerRadius(450)
+            }
+            Text("Jana Malibari")
+                .foregroundColor(.red)
+                .font(.largeTitle)
+                .bold()
+                .padding(.top,10)
+                .padding(.bottom,10)
+            Text("I'm 23 years old I like playing video games reading and watching movies and other stuff... Oh and My favorite Superhero is Spider-Man" )
+                }
         .padding()
     }
 }
