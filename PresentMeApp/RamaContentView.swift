573aafdba64f64890f65dc1bdf2c8bb835105ba5
Rama <ramabader891919@gmail.com>
2025-09-29 01:46:21 -0700

    Rama Change

---
+++ PresentMeApp/RamaContentView.swift
@@ -0,0 +1,18 @@
+//
+//  RamaContentView.swift
+//  PresentMeApp
+//
+//  Created by راما بدر بن جامع on 07/04/1447 AH.
+//
+
+import SwiftUI
+
+struct RamaContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RamaContentView()
+}
