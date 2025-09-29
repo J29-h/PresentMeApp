5e6040477222f66412c08adf6f2ce1032d64a6ea
najla <najlaalmuqati1@gmail.com>
2025-09-29 11:48:00 +0300

    Najla Change

---
+++ PresentMeApp/NajlaContentView.swift
@@ -0,0 +1,18 @@
+//
+//  NajlaContentView.swift
+//  PresentMeApp
+//
+//  Created by Najla Almuqati on 07/04/1447 AH.
+//
+
+import SwiftUI
+
+struct NajlaContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NajlaContentView()
+}
